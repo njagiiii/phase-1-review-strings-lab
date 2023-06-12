edd79f83f8d930f880df32b1c828a4bf96900e43
njagiiii <grace.makena@student.moringaschool.com>
2023-06-12 15:58:20 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+const currentUser ='Grace Makena';
+
+const welcomeMessage ='Welcome to Flatbook,  + Grace Makena + !';
+
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK,  + GRACE MAKENA+ !';
+
+const shortGreeting = 'Welcome,  + G + !' ; 
+       
